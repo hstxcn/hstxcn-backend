828c92c0181f63764063ac2534a1b7ee897ad0fb
minghuatang <hustmh@gmail.com>
2017-05-13 13:25:01 +0800

    change activate url

--- api/urls.py
+++ api/urls.py
@@ -33,7 +33,7 @@ urls = [
     (r"/login", "user.LoginHandler"),
     (r"/profile", "user.ProfileHandler"),
     (r"/user", "user.ProfileHandler"),
-    (r"/activate", "user.ActivateHandler"),
+    (r"/user/activate", "user.ActivateHandler"),
     (r"/user/(?P<uuid>[0-9a-fA-F]{32})/confirmation/(?P<token>.*)", "user.ConfirmationHandler"),
     (r"/user/(?P<uuid>[0-9a-fA-F]{32})/confirmation", "user.ResendConfirmationHandler"),
     (r"/users", "user.UserQueryHandler"),
